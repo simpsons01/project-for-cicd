7f72ab1cf5f481177666867ad3de277eb14f3eaa
simpsons01 <ilikekobe01@gmail.com>
2022-01-11 10:03:52 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+     {{ stag }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+const stag = process.env.VUE_APP_STAG
+export default defineComponent({
+  setup() {
+    return {
+      stag: ref(stag)
+    }
+  },
+})
+</script>
